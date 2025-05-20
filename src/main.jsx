8eeb545e9700834a79bc5333367015bab2e0d353
Ayman Shakil <aymanshakilbms@gmail.com>
2025-05-20 14:16:48 +0530

    update main component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { Suspense, lazy } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import Loading from "./components/Loading.jsx";
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const App = lazy(() => import("./App"));
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Suspense fallback={<Loading />}>
+      <ChakraProvider>
+        <Router>
+          <App />
+        </Router>
+      </ChakraProvider>
+    </Suspense>
+  </React.StrictMode>
+);
