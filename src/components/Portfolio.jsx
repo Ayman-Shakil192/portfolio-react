a20c65f038ccb3c295325f63bf60f9aa397b2456
Ayman Shakil <aymanshakilbms@gmail.com>
2025-05-20 14:18:28 +0530

    create Portfolio component

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Experience from "./Experience";
+import Projects from "./Projects";
+import About from "./About";
+import Skills from "./Skills";
+import Contact from "./Contact";
+
+export default function Portfolio() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Skills />
+      <Contact />
+    </div>
+  );
+}
